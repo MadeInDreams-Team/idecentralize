8b554e7d2f95fd49a05cf7f6e503f5aeeb6a472e
madeindreams <admin@madeindreams.ca>
2021-04-23 04:08:04 -0400

    Adding pages

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function({type}){
     if(type === 'table'){
-        return (<tbody className="text-center"><tr><td><img className="spinner" alt="Loading..." src="../logo.svg"></img></td></tr></tbody>)
+        return (<tbody className="text-center"><tr><td><img className="spinner" alt="Loading..." src="./logo.svg"></img></td></tr></tbody>)
     }else{
-        return(<div className="text-center"><img className="spinner" alt="Loading..." src="../logo.svg"></img> </div>)
+        return(<div className="text-center"><img className="spinner" alt="Loading..." src="./logo.svg"></img> </div>)
     }
 }
